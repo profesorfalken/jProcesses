ed3cce96eb342439834ef529fd7fbfa361b44cf8
Jan Høydahl <jan.git@cominvent.com>
2016-10-25 14:32:46 +0200

    Make the first date test so that it passes with the default hardcoded ENGLISH locale

--- src/test/java/org/jutils/jprocesses/util/ProcessesUtilsTest.java
+++ src/test/java/org/jutils/jprocesses/util/ProcessesUtilsTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class ProcessesUtilsTest {
   @Test
   public void getCustomDateFormat() throws Exception {
-      assertEquals("10/23/2016 08:30:00", ProcessesUtils.parseUnixLongTimeToFullDate("23 okt 08:30:00 2016"));
+      assertEquals("10/23/2016 08:30:00", ProcessesUtils.parseUnixLongTimeToFullDate("oct 23 08:30:00 2016"));
       try {
         ProcessesUtils.parseUnixLongTimeToFullDate("23 okt 2016 08:30:00");
         fail();
